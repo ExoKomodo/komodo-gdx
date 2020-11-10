f3f861ecea75e341c9b874c1bdaa3a27f3a2a599
James Orson <jamesaorson@gmail.com>
2020-11-09 22:19:58 -0800

    Use App extension for DesktopLauncher

--- desktop/src/com/exokomodo/komodo/desktop/DesktopLauncher.scala
+++ desktop/src/com/exokomodo/komodo/desktop/DesktopLauncher.scala
@@ -3,9 +3,7 @@ package com.exokomodo.komodo.desktop
 import com.badlogic.gdx.backends.lwjgl.{LwjglApplication, LwjglApplicationConfiguration}
 import com.exokomodo.komodo.Engine
 
-object DesktopLauncher {
-  def main(arg: Array[String]): Unit = {
-    val config = new LwjglApplicationConfiguration
-    new LwjglApplication(new Engine, config)
-  }
+object DesktopLauncher extends App {
+  val config = new LwjglApplicationConfiguration
+  new LwjglApplication(new Engine, config)
 }
